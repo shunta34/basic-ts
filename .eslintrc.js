9908f149b50174022af3af491646718dd9aff04e
shunta <s.goto@kaoiri.co.jp>
2021-05-23 17:14:41 +0900

    correct files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    // "prettier/@typescript-eslint"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  plugins: [
-    "@typescript-eslint"
-  ],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
   root: true,
-  rules: {}
+  rules: {},
 }
